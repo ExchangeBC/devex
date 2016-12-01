8525f12e7dc8625c55c84058165383d5d112b8f0
Chris Coldwell <chris.coldwell@gmail.com>
2016-12-01 08:48:27 -0800

    adding the list directive for projects

---
+++ modules/projects/client/directives/projects.client.directives.js
@@ -0,0 +1,136 @@
+(function () {
+	'use strict';
+	angular.module ('projects')
+	// -------------------------------------------------------------------------
+	//
+	// directive for listing projects
+	//
+	// -------------------------------------------------------------------------
+	.directive ('projectList', function () {
+		return {
+			restrict     : 'E',
+			controllerAs : 'vm',
+			scope        : {},
+			templateUrl  : '/modules/projects/client/views/list.projects.directive.html',
+			controller   : function ($scope, ProjectsService, Authentication) {
+				var vm = this;
+				console.log ('inside directive', Authentication);
+				vm.projects = ProjectsService.query ();
+			}
+		}
+	})
+	// -------------------------------------------------------------------------
+	//
+	// directive for listing project members
+	//
+	// -------------------------------------------------------------------------
+	.directive ('projectMemberList', function () {
+		return {
+			restrict     : 'E',
+			controllerAs : 'vm',
+			scope        : {
+				project: '='
+			},
+			templateUrl  : '/modules/projects/client/views/project.members.directive.html',
+			controller   : function ($scope, $rootScope, ProjectsService, Authentication) {
+				console.log ('Here we are in this directive and project._id = ',$scope.project._id);
+				var vm = this;
+				vm.project = $scope.project;
+				var reset = function () {
+					ProjectsService.getMembers ({
+						projectId: $scope.project._id
+					}).$promise.then (function (result) {
+						vm.members = result;
+						console.log ('Members:',result);
+						var columnLength = Math.floor (result.length / 2) + (result.length % 2);
+						vm.columns = [{
+							start : 0,
+							end   : columnLength
+						},{
+							start : columnLength,
+							end   : result.length
+						}];
+					});
+				}
+				vm.delete = function (userid, username) {
+					console.log ('delete user ', username, userid);
+					ProjectsService.denyMember ({
+						projectId: $scope.project._id,
+						userId: userid
+					}).$promise.then (function () {
+						$rootScope.$broadcast('updateMembers', 'done');
+					});
+				};
+				$rootScope.$on('updateMembers', function (event, message) {
+					reset ();
+				});
+				reset ();
+			}
+		}
+	})
+	// -------------------------------------------------------------------------
+	//
+	// directive for listing project member requests
+	//
+	// -------------------------------------------------------------------------
+	.directive ('projectMemberRequests', function () {
+		return {
+			restrict     : 'E',
+			controllerAs : 'vm',
+			scope        : {
+				project: '='
+			},
+			templateUrl  : '/modules/projects/client/views/projects.requests.directive.html',
+			controller   : function ($scope, $rootScope, ProjectsService, Authentication) {
+				var vm = this;
+				vm.project = $scope.project;
+				var reset = function () {
+					ProjectsService.getRequests ({
+						projectId: $scope.project._id
+					}).$promise.then (function (result) {
+						vm.members = result;
+						console.log ('Requests:',result);
+						var columnLength = Math.floor (result.length / 2) + (result.length % 2);
+						vm.columns = [{
+							start : 0,
+							end   : columnLength
+						},{
+							start : columnLength,
+							end   : result.length
+						}];
+					});
+				}
+				vm.confirm = function (userid, username) {
+					console.log ('confirm user ', username, userid);
+					ProjectsService.confirmMember ({
+						projectId: $scope.project._id,
+						userId: userid
+					}).$promise.then (function () {
+						// reset ();
+						$rootScope.$broadcast('updateMembers', 'done');
+					});
+				};
+				vm.deny = function (userid, username) {
+					console.log ('deny user ', username, userid);
+					ProjectsService.denyMember ({
+						projectId: $scope.project._id,
+						userId: userid
+					}).$promise.then (function () {
+						// reset ();
+						$rootScope.$broadcast('updateMembers', 'done');
+					});
+				};
+				$rootScope.$on('updateMembers', function (event, message) {
+					reset ();
+				});
+				reset ();
+			}
+		}
+	})
+	.filter ('slice', function () {
+		return function(arr, start, end) {
+			return arr.slice(start, end);
+		};
+	});
+	;
+}());
