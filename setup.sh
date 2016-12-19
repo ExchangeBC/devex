b03da6287cc68f54de213bae65a99e7140745e25
Andrew Whitehead <andrew@1crm.com>
2016-12-19 14:02:58 -0800

    optionally keep intermediate docker containers when building

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-docker run -p 27017:27017 -d --name db_devex mongo
-docker build -t mean/devex .
+if [[ $1 ]]; then
+	# pass non-zero value to preserve intermediate containers (faster rebuilds)
+	BUILD_ARGS="$BUILD_ARGS --rm=false"
+fi
+MONGO_PID=$(docker ps -a -q -f "name=db_devex")
+if [[ -z $MONGO_PID ]]; then
+	docker run -p 27017:27017 -d --name db_devex mongo
+fi
+docker build -t mean/devex $BUILD_ARGS .
