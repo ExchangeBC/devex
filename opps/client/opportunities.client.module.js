ed64be28c1861200ba9b6b8bdecbcf5c42c806c9
Chris Coldwell <chris.coldwell@gmail.com>
2017-01-25 20:19:39 -0800

    remove unnecessary directories

--- opps/client/opportunities.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('opportunities', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('opportunities.services');
-  app.registerModule('opportunities.routes', ['ui.router', 'core.routes', 'opportunities.services']);
-}(ApplicationConfiguration));
