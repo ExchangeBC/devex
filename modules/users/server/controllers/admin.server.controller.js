784430fce82ce1b48b96a4f60b3354cb20dce154
Chris Coldwell <chris.coldwell@gmail.com>
2017-03-15 14:27:50 -0700

    admin edit flags

--- modules/users/server/controllers/admin.server.controller.js
+++ modules/users/server/controllers/admin.server.controller.js
@@ -22,12 +22,15 @@ exports.update = function (req, res) {
   var user = req.model;
   // CC: USERFIELDS
   // For security purposes only merge these parameters
-  user.firstName = req.body.firstName;
-  user.lastName = req.body.lastName;
-  user.displayName = user.firstName + ' ' + user.lastName;
-  user.roles = req.body.roles;
-  user.government = req.body.government;
-  user.userTitle = req.body.userTitle;
+  user.firstName           = req.body.firstName;
+  user.lastName            = req.body.lastName;
+  user.displayName         = user.firstName + ' ' + user.lastName;
+  user.roles               = req.body.roles;
+  user.government          = req.body.government;
+  user.userTitle           = req.body.userTitle;
+  user.notifyOpportunities = req.body.notifyOpportunities;
+  user.notifyEvents        = req.body.notifyEvents;
+  user.notifyBlogs         = req.body.notifyBlogs;
 
   user.save(function (err) {
     if (err) {
