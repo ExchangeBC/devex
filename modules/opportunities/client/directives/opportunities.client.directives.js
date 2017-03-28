b855e259e5708df6d45e792cdd6985437b5e5b8f
Chris Gat <chris.gat@gmail.com>
2017-03-28 14:23:31 -0700

    feat(opportunities): add ability to disable opportunity notifications on the front end
    There are 3 situations where a notification can be sent
    1) A user publishes an opportunity on the opportunity lists page
    2) A user publishes an opportunity on an opportunity page
    3) A user update a property of a published opportunity
    
    For first time publishing, notifications are sent and the user is not prompted whether they want to send notifications or not.
    
    On subsequent publications, the user is prompted if they would like to send notifications or not.

--- modules/opportunities/client/directives/opportunities.client.directives.js
+++ modules/opportunities/client/directives/opportunities.client.directives.js
@@ -17,7 +17,7 @@
 				context: '@'
 			},
 			templateUrl  : '/modules/opportunities/client/views/list.opportunities.directive.html',
-			controller   : function ($scope, OpportunitiesService, Authentication, Notification) {
+			controller   : function ($scope, OpportunitiesService, Authentication, Notification, modalService, $q) {
 				var rightNow = new Date ();
 				var vm     = this;
 				var isUser = Authentication.user;
@@ -65,10 +65,40 @@
 					var publishedState = opportunity.isPublished;
 					var t = state ? 'Published' : 'Un-Published'
 					opportunity.isPublished = state;
-					opportunity.createOrUpdate ()
-					//
+
+					opportunity.doNotNotify = false;
+					var modalOptions = {
+			        closeButtonText: 'Do Not Send Notification',
+			        actionButtonText: 'Send Notification',
+			        headerText: 'Publish Opportunity',
+			        bodyText: 'You are re-publishing this opportunity. Would you like to re-notify all subscribed users?'
+			    };
+			    var promise;
+			    //
+			    // Bypass the modal if first time publishing OR if unpublishing
+			    //
+			    if ((opportunity.isPublished && !opportunity.lastPublished) ||
+			    			!opportunity.isPublished) {
+			    	promise = $q.resolve();
+			    }
+			    else {
+			    	promise = modalService.showModal({}, modalOptions)
+		        .then(function sendNotification (result) {
+		        		opportunity.doNotNotify = false;
+		        		console.log('send notify callback')
+		        },
+		        function doNotSendNotificaiton (result) {
+		        	opportunity.doNotNotify = true;
+		        	console.log('do not send notify callback')
+		        })
+			    }
+
+	        //
 					// success, notify and return to list
 					//
+	        promise.then(function() {
+	        	return opportunity.createOrUpdate();
+	        })
 					.then (function (res) {
 						Notification.success ({
 							message : '<i class="glyphicon glyphicon-ok"></i> Opportunity '+t+' Successfully!'
