724f7ea94e4048a8ac456dfecbb0c65c4894b9cb
Chris Gat <chris.gat@gmail.com>
2017-03-31 13:46:05 -0700

    clean up commented out method in notifier

--- modules/core/server/controllers/core.server.notifier.js
+++ modules/core/server/controllers/core.server.notifier.js
@@ -44,40 +44,6 @@ exports.notifier = function (notifyBChost, port, serviceName, type) {
           return res.json();
         });
     },
-    // unsubscribeByEmail : function (userId) {
-    //   console.log('unsubscribe');
-
-    //   var whereFilter = JSON.stringify({
-    //       'where':
-    //         {
-    //           'userId': userId.toString(),
-    //           'serviceName': serviceName,
-    //           'channel': type
-    //         }
-    //     });
-
-    //   console.log(whereFilter);
-    //   // we fetch all subscriptions that match the userId, serviceName, and type
-    //   // then delete each of these (there should be one, but just in case)
-    //   return fetch(url + '/api/subscriptions?filter=' + whereFilter, {
-    //     method: 'get',
-    //     headers: { 'Accept': 'application/json'}
-    //     })
-    //     .then(function(res) {
-    //       console.log('RESULTS')
-    //       return res.json();
-    //     })
-    //     .then(function(json) {
-    //       json.forEach(function(subscription) {
-    //         fetch('http:://10.0.0.188:8888/api/subscriptions', {
-    //           method: 'delete'
-    //         })
-    //       })
-    //     })
-    //     .catch(function(err) {
-    //       console.log('err:' + err);
-    //     });
-    // },
     notify : function (messageObj) {
       return fetch(url + '/api/notifications/', {
         method: 'post',
