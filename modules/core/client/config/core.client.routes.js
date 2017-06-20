b71112a7b8ddeb7b91c25d5eaccc6b883d3a24ae
Chris Coldwell <chris.coldwell@gmail.com>
2017-06-20 12:58:50 -0700

    copied over roadmap html into /roadmap

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -112,6 +112,14 @@
           pageTitle: 'Code With Us'
         }
       })
+      .state('roadmap', {
+        url: '/roadmap',
+        templateUrl: '/modules/core/client/views/roadmap.view.html',
+        data: {
+          ignoreState: true,
+          pageTitle: 'Roadmap'
+        }
+      })
       .state('iotblog', {
         url: '/iotblog',
         templateUrl: '/modules/core/client/views/iotblog.view.html',
