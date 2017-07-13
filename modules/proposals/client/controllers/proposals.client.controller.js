4155b35d33b8ec7a18b38b5dae2f1421d9436af1
Chris Coldwell <chris.coldwell@gmail.com>
2017-07-13 09:56:59 -0700

    partway through little changes

--- modules/proposals/client/controllers/proposals.client.controller.js
+++ modules/proposals/client/controllers/proposals.client.controller.js
@@ -41,6 +41,15 @@
 		console.log ('ppp.proposal', ppp.proposal);
 		ppp.user          = angular.copy (Authentication.user);
 		var pristineUser = angular.toJson(Authentication.user);
+		ppp.tinymceOptions = {
+			resize      : true,
+			width       : '100%',  // I *think* its a number and not '400' string
+			height      : 100,
+			menubar     : '',
+			elementpath : false,
+			plugins     : 'textcolor lists advlist link',
+			toolbar     : 'undo redo | styleselect | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link | forecolor backcolor'
+		};
 		if (!opportunity) {
 			console.error ('no opportunity was provided!');
 		}
