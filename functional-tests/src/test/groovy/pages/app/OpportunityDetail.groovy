65c79a1d35a4af4b000eff8085ac0517ebf71048
GeorgeWalker <george.walker@quartech.com>
2017-08-09 12:40:22 -0700

    Updated BDD tests

---
+++ functional-tests/src/test/groovy/pages/app/OpportunityDetail.groovy
@@ -0,0 +1,8 @@
+package pages.app
+
+import geb.Page
+
+class OpportunityDetail extends Page {
+    static at = { $("h1", text:"DEVELOPING THE TRANSPORTATION FUELS REPORTING SYSTEM WEBSITE BACK-END (BCDX-MEM-003)") }
+    static url = "/opportunities/opp-developing-the-transportation-fuels-reporting-system-website-back-end--bcdx-mem-003-"
+}
