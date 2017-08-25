5a1b436d251c12663aa59cc71110a05b3fa572ea
GeorgeWalker <george.walker@quartech.com>
2017-08-25 14:39:06 -0700

    Updated BDD tests

--- functional-tests/src/test/groovy/FlowSpecs.groovy
+++ functional-tests/src/test/groovy/FlowSpecs.groovy
@@ -1,8 +1,12 @@
 import geb.spock.GebReportingSpec
 import pages.app.HomePage
 import pages.app.Disclaimer
+import pages.app.SignedIn
 import pages.app.OpportunityDetail
 import spock.lang.Unroll
+
+
+import javax.xml.crypto.dsig.SignedInfo
 
 
 class FlowSpecs extends GebReportingSpec {
@@ -28,4 +32,53 @@ class FlowSpecs extends GebReportingSpec {
         expect:
             assert $('div','class':'well-transparent')[0].displayed  == true
     }
+
+
+    def "4 Proposal Workflow Model" (){
+
+        /**
+         * TODO:  Right now this test is dependent on a certain opportunity to be present in the system.
+         * The test should be extended to include logging in as an admin and creating an opportunity
+         * This way the test will not fail if the specific opportunity referenced in the test is not present.
+         */
+
+        /* Start by going to the home page */
+        given:
+            to HomePage
+        when:
+
+            /* Click the sign in link */
+            $("a","ui-sref":"authentication.signin").click()
+            /* Wait for the github login feature to appear */
+            waitFor { $("a", "ng-click":"vm.callOauthProvider('/api/auth/github')")}
+            /* Click the Github login link */
+            $("a", "ng-click":"vm.callOauthProvider('/api/auth/github')")[0].click()
+            /* Fill in the details */
+            $("input", id:"login_field").value( System.getenv("TEST_USERNAME"))
+            $("input", id:"password").value(System.getenv("TEST_PASSWORD"))
+            /* Login */
+            $("input", name:"commit").click()
+        then:
+            /* Wait for the login process to complete */
+            at SignedIn
+            waitFor { $("span", "ng-bind":"vm.authentication.user.displayName")[0].text() != "" }
+        expect:
+            assert { $("span", "ng-bind":"vm.authentication.user.displayName")[0].text() != "" }
+
+        /* Apply for the opportunity */
+        and:
+           to OpportunityDetail
+        when:
+            $("a",class:"btn btn-lg btn-warning").click()
+            waitFor { $("div",class:"modal-body").displayed == true }
+        then:
+            /* check that the required fields exist. */
+            assert ($("input","name":"firstName").displayed == true)
+            assert ($("input","name":"lastName").displayed == true)
+            assert ($("input","name":"email").displayed == true)
+            assert ($("input","name":"phone").displayed == true)
+
+    }
+
+
 }
--- functional-tests/src/test/groovy/pages/app/OpportunityDetail.groovy
+++ functional-tests/src/test/groovy/pages/app/OpportunityDetail.groovy
@@ -3,6 +3,6 @@ package pages.app
 import geb.Page
 
 class OpportunityDetail extends Page {
-    static at = { $("h1", text:"DEVELOPING THE TRANSPORTATION FUELS REPORTING SYSTEM WEBSITE BACK-END (BCDX-MEM-003)") }
-    static url = "/opportunities/opp-developing-the-transportation-fuels-reporting-system-website-back-end--bcdx-mem-003-"
+    static at = { $("h1", text:"Show that BDD testing works!") }
+    static url = "/opportunities/opp-show-that-bdd-testing-works-"
 }
