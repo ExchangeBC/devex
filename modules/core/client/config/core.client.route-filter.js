41b4cb1e30a356df271b768fb4c0c75ac78fcd9f
Chris Coldwell <chris.coldwell@gmail.com>
2017-09-11 13:16:37 -0700

    changed ambiguous var declarations

--- modules/core/client/config/core.client.route-filter.js
+++ modules/core/client/config/core.client.route-filter.js
@@ -11,15 +11,17 @@
 		$rootScope.$on('$stateChangeStart', stateChangeStart);
 		$rootScope.$on('$stateChangeSuccess', stateChangeSuccess);
 
-		function stateChangeStart(event, toState, toParams, fromState, fromParams) {
+		function stateChangeStart(event, toState, toParams) {
 			// Check authentication before changing state
 			var userroles   = (Authentication.user && Authentication.user.roles !== undefined) ? Authentication.user.roles : ['guest'];
 			var hasroles    = (toState.data && toState.data.roles && toState.data.roles.length > 0);
 			var hasnotroles = (toState.data && toState.data.notroles && toState.data.notroles.length > 0);
 			var allowed     = true;
+			var roles;
+			var i;
 			if (hasroles) {
 				allowed = false;
-				for (var i = 0, roles = toState.data.roles; i < roles.length; i++) {
+				for (i = 0, roles = toState.data.roles; i < roles.length; i++) {
 					if ((roles[i] === 'guest') || (!!~userroles.indexOf(roles[i]))) {
 						allowed = true;
 						break;
@@ -27,7 +29,7 @@
 				}
 			}
 			if (allowed && hasnotroles) {
-				for (var i = 0, roles = toState.data.notroles; i < roles.length; i++) {
+				for (i = 0, roles = toState.data.notroles; i < roles.length; i++) {
 					if (!!~userroles.indexOf(roles[i])) {
 						allowed = false;
 						break;
