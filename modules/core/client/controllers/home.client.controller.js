fd5f5cc293404675f59cc83f1d92526f88802d8c
Chris Coldwell <chris.coldwell@gmail.com>
2017-09-11 13:12:58 -0700

    fix isUser missing so wording always appeared regardless of login status

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,8 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  function HomeController() {
+  function HomeController(Authentication) {
     var vm = this;
+    vm.isUser = Authentication.user;
   }
 }());
