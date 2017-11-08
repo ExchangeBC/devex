ba65227a734115e1fbbdcf09aeee15e82098a7bf
Roland Stens <roland.stens@gmail.com>
2017-11-07 20:53:49 -0800

    HomePage Update

--- functional-tests/src/test/groovy/pages/app/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/app/HomePage.groovy
@@ -22,28 +22,29 @@ class HomePage extends Page {
 		ProjectsLink { PositionAndClick("a","projects.list") }
 		ProgramsLink { PositionAndClick("a","programs.list") }
 		SigninLink { PositionAndClick("a","authentication.signin") }
-		SignUpMidPageLink { PositionAndClick("a","authentication.gov") }
-		AboutMidPageLink { $("a", text:"Learn More") }
+		SignUpMidPageLink { PositionAndClick("a","authentication.gov",1) }
+		
+// Folowing links are not yet operational
 		LearnMoreLink { $("a", href:"/codewithus") }
-
-
+		AboutMidPageLink { $("a", text:"Learn More") }
 		AdminGovsLink { $() } //Admin
 		AdminNotifymeetsLink { $() } //Admin
 		AdminNotifyoppsLink { $() } //Admin
 		AdminUsersLink { $() } //Admin
 		ApiAuthSignoutLink { $() }
-		 //Goes to codewithusPage
 		NotificationsLink { $() } //Admin
 		ProfilesLink { PositionAndClick("a","profiles.list") }
 		ProposalsLink { $("a", "ui-sref":"proposals.list({})") }
-
 		SettingsPictureLink { $() } //Admin
 		SettingsProfileLink { $() } //Admin
     }
 
-    Boolean PositionAndClick(String elementType, String elementId){
+    // Since webdriver does not want to click on non-visible links,
+    // we have to position the test page show the links before we can click on it.
+    // Currently this function only support links with an id.
+    Boolean PositionAndClick(String elementType, String elementId, int index=0) {
     		js.exec('document.getElementById("' + elementId + '").scrollIntoView(true);')
-			$("$elementType", id:"$elementId")[0].click()
+			$("$elementType", id:"$elementId")[index].click()
 			return true
     }
 }
