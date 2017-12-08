2de2df00fba9c5d29de3643ca0ae52930f898de3
Roland Stens <roland.stens@gmail.com>
2017-12-08 12:49:51 -0800

    flowspecs update

---
+++ functional-tests/src/test/groovy/FlowSpecs.groovy
@@ -0,0 +1,76 @@
+import geb.spock.GebReportingSpec
+
+import pages.app.AboutPage
+import pages.app.AccessibilityPage
+import pages.app.AdminGovsPage
+import pages.app.AdminNotifymeetsPage
+import pages.app.AdminNotifyoppsPage
+import pages.app.AdminUsersPage
+import pages.app.ApiAuthSignoutPage
+import pages.app.AuthenticationGovernmentPage
+import pages.app.AuthenticationSigninPage
+import pages.app.AuthenticationSigninadminPage
+import pages.app.CodewithusPage
+import pages.app.CopyrightPage
+import pages.app.DisclaimerPage
+import pages.app.HomePage
+import pages.app.HomePage
+import pages.app.NotificationsPage
+import pages.app.OpportunitiesPage
+import pages.app.OpportunityDetailPage
+import pages.app.OrgsListPage
+import pages.app.PrivacyPage
+import pages.app.ProfilesPage
+import pages.app.ProgramsPage
+import pages.app.ProjectsPage
+import pages.app.ProposalsPage
+import pages.app.RoadmapPage
+import pages.app.SettingsPicturePage
+import pages.app.SettingsProfilePage
+
+import spock.lang.Unroll
+import spock.lang.Title
+
+@Title("Basic Link Checker to verify that the application is up an running.")
+class FlowSpecs extends GebReportingSpec {
+
+  @Unroll
+  def "Navigate Page from: #StartPage, click Link: #ClickLink, Assert Page: #AssertPage"(){
+    given: "I start on the #StartPage"
+		to StartPage
+    when: "I click on the link #ClickLink"
+       page."$ClickLink"
+    then: "I arrive on the #AssertPage page"
+	     at AssertPage
+    where:
+    StartPage | ClickLink               || AssertPage 
+    HomePage  | "AboutLink"             || AboutPage
+    HomePage  | "AccessibilityLink"     || AccessibilityPage
+    HomePage  | "CodewithusLink"        || CodewithusPage
+    HomePage  | "CopyrightLink"         || CopyrightPage
+    HomePage  | "DisclaimerLink"        || DisclaimerPage
+    HomePage  | "PrivacyLink"           || PrivacyPage
+    HomePage  | "RoadmapLink"           || RoadmapPage
+    HomePage  | "SigninadminLink"       || AuthenticationSigninadminPage        
+    HomePage  | "OpportunitiesMenuLink" || OpportunitiesPage
+    HomePage  | "ProjectsLink"          || ProjectsPage
+    HomePage  | "ProgramsLink"          || ProgramsPage
+    HomePage  | "SigninLink"            || AuthenticationSigninPage
+    HomePage  | "SignUpMidPageLink"     || AuthenticationGovernmentPage
+    HomePage  | "OrgsList"              || OrgsListPage
+
+// For future expansion
+    //HomePage | "AboutMidPageLink" || AboutPage
+    //HomePage | "LearnMoreLink" || CodewithusPage
+    //HomePage | "AdminGovsLink" || AdminGovsPage
+    //HomePage | "AdminNotifymeetsLink" || AdminNotifymeetsPage
+    //HomePage | "AdminNotifyoppsLink" || AdminNotifyoppsPage
+    //HomePage | "AdminUsersLink" || AdminUsersPage
+    //HomePage | "ApiAuthSignoutLink" || ApiAuthSignoutPage
+    //HomePage | "NotificationsLink" || NotificationsPage
+    //HomePage | "ProfilesLink" || ProfilesPage
+    //HomePage | "ProposalsLink" || ProposalsPage
+    //HomePage | "SettingsPictureLink" || SettingsPicturePage
+    //HomePage | "SettingsProfileLink" || SettingsProfilePage
+ }
+}
