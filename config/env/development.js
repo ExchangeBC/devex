18f2df5f75ed54ee64dd0f2e99287e762b732bd0
Chris Coldwell <chris.coldwell@gmail.com>
2018-02-15 14:43:22 -0800

    cc:logging

--- config/env/development.js
+++ config/env/development.js
@@ -15,7 +15,10 @@ module.exports = {
   log: {
     // logging with Morgan - https://github.com/expressjs/morgan
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: 'dev',
+    //
+    // cc:logging: modified apache format including internal user identification
+    //
+    format: 'tiny',
     fileLogger: {
       directoryPath: process.cwd(),
       fileName: 'app.log',
