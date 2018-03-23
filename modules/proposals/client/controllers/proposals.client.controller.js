1828cdea5b0c2bbb4a2d58bc58934a01f5bd7f9e
Akiff Manji <amanji@localhost.localdomain>
2018-03-23 06:26:47 -0700

    No longer require modalService and modal options

--- modules/proposals/client/controllers/proposals.client.controller.js
+++ modules/proposals/client/controllers/proposals.client.controller.js
@@ -126,7 +126,7 @@
 	// Controller the view of the proposal page
 	//
 	// =========================================================================
-	.controller ('ProposalEditController', function (uibButtonConfig, capabilities, editing, $scope, $sce, ask, Upload, $state, $stateParams, proposal, opportunity, Authentication, ProposalsService, UsersService, Notification, NotificationsService, modalService, dataService, org) {
+	.controller ('ProposalEditController', function (uibButtonConfig, capabilities, editing, $scope, $sce, ask, Upload, $state, $stateParams, proposal, opportunity, Authentication, ProposalsService, UsersService, Notification, NotificationsService, dataService, org) {
 		var isInArray = function (a,el) {return a.map (function(al){return (el===al);}).reduce(function(a,c){return (a||c);},false); };
 		var ppp                                   = this;
 		ppp.features                              = window.features;
@@ -279,17 +279,6 @@
 			else if (status === 'Draft') return 'Submit';
 			else if (status === 'Submitted') return 'label-success';
 		}
-		// -------------------------------------------------------------------------
-		//
-		// things to do with leaving the form without saving
-		//
-		// -------------------------------------------------------------------------
-		var saveChangesModalOpt = {
-			closeButtonText: 'Return To Proposal',
-			actionButtonText: 'Continue',
-			headerText: 'Unsaved Changes!',
-			bodyText: 'You have unsaved changes. Changes will be discarded if you continue.'
-		};
 		// -------------------------------------------------------------------------
 		//
 		// team score
