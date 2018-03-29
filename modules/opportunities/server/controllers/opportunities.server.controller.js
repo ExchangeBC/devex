214261add6251cd6537a143feb49839ec27057ed
Mark Wilson <Mark.Wilson@gov.bc.ca>
2018-03-29 14:27:31 -0700

    Revert "BA-188 / Attempt 2"
    This reverts commit 51082daf184feafa042bdbb5629acab09e265f99.

--- modules/opportunities/server/controllers/opportunities.server.controller.js
+++ modules/opportunities/server/controllers/opportunities.server.controller.js
@@ -238,18 +238,18 @@ var oppBody = function (opp) {
 		mixed   : 'Some in-person work required'
 	}
 	var ret = '';
-	ret += 'Value: '+earn+<br>;
-	ret += 'Closes: '+deadline+<br>;
-	ret += 'Location: '+opp.location+' '+locs[opp.onsite]+<br>;
-	ret += '<h3>Opportunity Description</h3>';
+	ret += 'Value: '+earn;
+	ret += 'Closes: '+deadline;
+	ret += 'Location: '+opp.location+' '+locs[opp.onsite];
+	ret += '<h2>Opportunity Description</h2>';
 	ret += opp.description;
-	ret += '<h3>Acceptance Criteria</h3>';
+	ret += '<h2>Acceptance Criteria</h2>';
 	ret += opp.criteria;
-	ret += '<h3>How to Apply</h3>';
-	ret += '<p><a href="https://bcdevexchange.org/opportunities/'+opp.code+'">Visit the opportunity page on the BCDevExchange</a> and submit a proposal by <b>16:00 Pacific Time on '+deadline+'</b>.</p>';
+	ret += '<h2>How to Apply</h2>';
+	ret += '<p>Go to the <a href="https://bcdevexchange.org/opportunities/'+opp.code+'">Opportunity Page</a>, click the Apply button above and submit your proposal by 16:00 PST on '+deadline+'</b>.</p>';
 	ret += '<p>We plan to assign this opportunity by <b>'+assignment+'</b> with work to start on <b>'+start+'</b>.</p>';
 	// ret += '<p>If your proposal is accepted and you are assigned to the opportunity, you will be notified by email and asked to confirm your agreement to the <a href="https://github.com/BCDevExchange/devex/raw/master/Code-with-Us%20Terms_BC%20Developers%20Exchange.pdf"><i>Code With Us</i> terms and contract.</a></p>';
-	ret += '<h3>Proposal Evaluation Criteria</h3>';
+	ret += '<h2>Proposal Evaluation Criteria</h2>';
 	ret += opp.evaluation;
 	return ret;
 };
