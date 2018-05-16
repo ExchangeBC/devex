c41da06c8a998e50a6819a3dcf3dbe70a86522be
Andrew Sutherland <sutherlanda@gmail.com>
2018-05-16 13:12:18 -0700

    BA-611 - Remove unnessary assert, fix credentials for dev env

--- functional-tests/src/test/groovy/OpportunityCreateSpecs.groovy
+++ functional-tests/src/test/groovy/OpportunityCreateSpecs.groovy
@@ -62,16 +62,16 @@ class OpportunityCreateSpecs extends GebReportingSpec {
 
 
               then:
-              at ProgramViewPage
+              waitFor { at ProgramViewPage }
               assert PublishButton
 
               when: "I click the publish button"
               PublishButton.click()
 
               then:
-              waitFor {at ProgramViewPage }
+              waitFor { at ProgramViewPage }
               assert UnpublishButton
-              assert UnpublishButton.isDisplayed()
+              //assert UnpublishButton.isDisplayed()
 
          where:
           ProgramTitleValue | ShortDescriptionValue | DescriptionValue | WebsiteValue
