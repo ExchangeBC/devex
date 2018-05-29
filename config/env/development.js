1230e2f07d9431d7d84de82da325c11202a6cb20
Andrew Sutherland <sutherlanda@gmail.com>
2018-05-29 14:28:00 -0700

    BA-656 - Updated dev config with working mailer variables

--- config/env/development.js
+++ config/env/development.js
@@ -68,13 +68,12 @@ module.exports = {
     sandbox: true
   },
   mailer: {
-    from: process.env.MAILER_FROM || 'MAILER_FROM',
+    from: process.env.MAILER_FROM || '"BC Developers Exchange" <noreply@bcdevexchange.org>',
     options: {
       service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
-      auth: {
-        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
-      }
+      host: 'apps.smtp.gov.bc.ca',
+      ignoreTLS: true,
+      secure: false
     }
   },
   shared: {
