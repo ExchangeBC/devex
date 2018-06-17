a3aabb5f4964a01839009d12f2149ddf73b8b2af
Andrew Sutherland <sutherlanda@gmail.com>
2018-06-16 21:40:45 -0700

    Hotfix - Rollback of nodemailer

--- config/env/development.js
+++ config/env/development.js
@@ -73,12 +73,7 @@ module.exports = {
       service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
       host: 'apps.smtp.gov.bc.ca',
       ignoreTLS: true,
-      secure: true,
-      port: 25,
-      auth: {
-        user: process.env.MAILER_EMAIL_ID,
-        pass: process.env.MAILER_PASSWORD
-      }
+      secure: false
     }
   },
   shared: {
