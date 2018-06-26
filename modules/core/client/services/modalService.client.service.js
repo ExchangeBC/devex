da252c8d65173860dd42158bd3bb81ed32417d27
Andrew Sutherland <sutherlanda@gmail.com>
2018-06-26 16:32:30 -0700

    BA-686 - Adjustments to modal styling
    - Add default modal size to modal service

--- modules/core/client/services/modalService.client.service.js
+++ modules/core/client/services/modalService.client.service.js
@@ -5,6 +5,7 @@
 	.service('modalService', ['$uibModal', function ($uibModal) {
 		// inspired by this post :https://weblogs.asp.net/dwahlin/building-an-angularjs-modal-service
 		var modalDefaults = {
+			size: 'sm',
 			backdrop: true,
 			keyboard: true,
 			modalFade: true,
