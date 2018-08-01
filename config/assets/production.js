5307a677d47fa6ec8d1243a58f77b8503e7faa52
Andrew Sutherland <sutherlanda@gmail.com>
2018-07-31 19:58:50 -0700

    BA-802 - Add sample .env file
    - Add document .env sample
    - Small correction to prevent app from failing to start
    if MONGO_SEED = false

--- config/assets/production.js
+++ config/assets/production.js
@@ -32,7 +32,7 @@ module.exports = {
         'public/lib/ng-img-crop/compile/minified/ng-img-crop.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
         'public/lib/angular-drag-and-drop-lists/angular-drag-and-drop-lists.min.js',
-		'public/lib/ng-idle/angular-idle.min.js'
+        'public/lib/ng-idle/angular-idle.min.js',
         // endbower
       ]
     },
