91eb0705f0aaff3498900875f4d779d5bcc662dd
Alejandro Sanchez <emailforasr@gmail.com>
2018-11-05 23:17:52 -0800

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,158 @@
-const glob = require('glob');
-const path = require('path');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge');
 
-module.exports = {
-	entry: {
-		vendor: [
-			'angular',
-			'angular-animate',
-			'angular-breadcrumb',
-			'angular-messages',
-			'angular-mocks',
-			'angular-resource',
-			'angular-ui-notification',
-			'angular-ui-router',
-			'angular-ui-tinymce',
-			'ng-img-crop',
-			'ng-file-upload',
-			'ng-idle',
-			'angular-drag-and-drop-lists',
-			'angular-sanitize',
-			'jquery',
-			'bootstrap',
-			'ui-bootstrap4'
+const path = require('path');
+const glob = require('glob');
+const minimatch = require('minimatch');
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
+
+// various webpack goodies
+const parts = require('./webpack.parts');
+
+// application-specific PATHS
+const paths = require('./paths');
+
+const DEVELOPMENT = 'development';
+const PRODUCTION = 'production';
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
+const envFile = {};
+envFile['BASE_PATH'] = JSON.stringify('');
+// Populate the env dict with Environment variables from the system
+if (process.env) {
+	Object.keys(process.env).map(key => {
+		envFile[key] = JSON.stringify(process.env[key]);
+	});
+}
+// Update the env dict with any in the .env file
+if (dotenv.parsed) {
+	Object.keys(dotenv.parsed).map(key => {
+		envFile[key] = JSON.stringify(dotenv.parsed[key]);
+	});
+}
+
+const BUILD_FILE_NAMES = {
+	css: '[name].css',
+	bundle: '[name].bundle.js',
+	vendor: '[id].[chunkhash:4].js',
+	assets: 'assets/[name].[hash:4].[ext]',
+};
+
+const commonConfig = merge([
+	{
+		entry: {
+			vendor: [
+				'angular',
+				'angular-animate',
+				'angular-breadcrumb',
+				'angular-messages',
+				'angular-mocks',
+				'angular-resource',
+				'angular-ui-notification',
+				'angular-ui-router',
+				'angular-ui-tinymce',
+				'ng-img-crop',
+				'ng-file-upload',
+				'ng-idle',
+				'angular-drag-and-drop-lists',
+				'angular-sanitize',
+				'jquery',
+				'bootstrap',
+				'ui-bootstrap4'
+			],
+			appConfig: './modules/core/client/app/config.js',
+			appInit: './modules/core/client/app/init.js',
+			moduleConfig: glob.sync('./modules/*/client/*.js'),
+			modules: glob.sync('./modules/*/client/**/*.js', {
+				ignore: [
+					'./modules/*/client/*.js',
+					'./main.js',
+					'./modules/core/client/app/config.js',
+					'./modules/core/client/app/init.js'
+				]
+			})
+		},
+		resolve: {
+			// These files are tried when trying to resolve a directory...
+			//  - i.e. require("/some/dir") -> require("some/dir/index.js")
+			mainFiles: ['index', 'compile/minified/ng-img-crop']
+		},
+	},
+	parts.setEnvironmentVariables(envFile),
+	parts.loadFonts({
+		urlLoaderOptions: {
+			// Inline files smaller than 10 kB (10240 bytes)
+			limit: 10 * 1024,
+		}
+	}),
+	{
+		plugins: [
+			new webpack.ProvidePlugin({
+				jQuery: 'jquery'
+			}),
 		],
-		appConfig: './modules/core/client/app/config.js',
-		appInit: './modules/core/client/app/init.js',
-		moduleConfig: glob.sync('./modules/*/client/*.js'),
-		modules: glob.sync('./modules/*/client/**/*.js', {
-			ignore: [
-				'./modules/*/client/*.js',
-				'./main.js',
-				'./modules/core/client/app/config.js',
-				'./modules/core/client/app/init.js'
-			]
-		})
+	}
+]);
+
+const devConfig = merge([
+	{
+		output: {
+			path: paths.build,
+			publicPath: ASSET_PATH,
+			filename: BUILD_FILE_NAMES.bundle,
+		},
 	},
-	output: {
-		path: path.join(__dirname, './public/dist'),
-		filename: '[name].bundle.js'
+	parts.generateSourceMaps({
+		type: "source-map",
+	}),
+	parts.devServer({
+		// Parse host and port from env to allow customization.
+		//
+		// If you use Docker, set
+		// host: HOST || "0.0.0.0";
+		//
+		// 0.0.0.0 is available to all network devices
+		// unlike default `localhost`.
+		host: HOST || '0.0.0.0',
+		port: PORT,
+	}),
+	parts.loadCSS(),
+	parts.loadTinyMCE(),
+	parts.loadImages({
+		urlLoaderOptions: {
+			// Inline files smaller than 10 kB (10240 bytes)
+			limit: 10 * 1024,
+		}
+	}),
+]);
+
+const prodConfig = merge([
+	{
+		output: {
+			path: paths.build,
+			publicPath: ASSET_PATH,
+			chunkFilename: BUILD_FILE_NAMES.vendor,
+			filename: BUILD_FILE_NAMES.bundle,
+		},
 	},
-	devtool: 'source-map',
-	resolve: {
-		mainFiles: ['index', 'compile/minified/ng-img-crop']
-	},
-	optimization: {
-		minimize: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
-			},
-			{
-				test: /\.less$/,
-				use: [{ loader: MiniCssExtractPlugin.loader }, { loader: 'css-loader' }, { loader: 'less-loader' }]
-			},
-			{
-				test: require.resolve('tinymce/tinymce'),
-				use: ['imports-loader?this=>window', 'exports-loader?window.tinymce']
-			},
-			{
-				test: /tinymce\/(themes|plugins)\//,
-				use: ['imports-loader?this=>window']
-			},
-			{
-				test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-				loader: 'url-loader?limit=10000'
-			},
-			{
-				test: /\.(eot|ttf|wav|mp3)$/,
-				loader: 'file-loader'
-			},
-			{
-				test: /\.scss$/,
-				use: [{ loader: MiniCssExtractPlugin.loader }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
-			}
-		]
-	},
-	node: {
-		console: true,
-		fs: 'empty',
-		net: 'empty',
-		tls: 'empty'
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			jQuery: 'jquery'
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	]
-	// stats: 'errors-only'
+	parts.clean(paths.build),
+	parts.extractCSS({
+		filename: BUILD_FILE_NAMES.css,
+	}),
+	parts.loadTinyMCE(),
+	parts.loadImages({
+		urlLoaderOptions: {
+			// Inline files smaller than 10 kB (10240 bytes)
+			limit: 10 * 1024,
+		},
+	}),
+	parts.minifyJS(),
+]);
+
+module.exports = (mode) => {
+	// return env-specific configuration - i.e. dev vs prod
+	const config = mode === PRODUCTION ? prodConfig : devConfig;
+	return merge(commonConfig, config, { mode });
 };
---
+++ webpack.parts.js
@@ -0,0 +1,220 @@
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const postCSSLoader = {
+	loader: 'postcss-loader',
+	options: {
+		plugins: () => ([autoprefixer]),
+	},
+};
+
+exports.devServer = ({ host, port } = {}) => ({
+	devServer: {
+		historyApiFallback: true,
+		stats: 'errors-only',
+		host,
+		port,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+		},
+		overlay: {
+			errors: true,
+			warnings: true,
+		},
+	},
+});
+
+exports.loadJS = ({ include, exclude } = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include,
+				exclude,
+				loader: 'babel-loader',
+			},
+		],
+	},
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				include,
+				exclude,
+				use: [
+					'style-loader',
+					'css-loader',
+					postCSSLoader,
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.less$/,
+				include,
+				exclude,
+				use: [
+					'style-loader',
+					'css-loader',
+					postCSSLoader,
+					'less-loader',
+				],
+			},
+		],
+	}
+});
+
+exports.extractCSS = ({ include, exclude, filename } = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				include,
+				exclude,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.less$/,
+				include,
+				exclude,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'less-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename,
+		}),
+	],
+});
+
+exports.loadImages = ({ include, exclude, urlLoaderOptions, fileLoaderOptions, imageLoaderOptions } = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.(gif|png|jpe?g)$/,
+				include,
+				exclude,
+				loader: 'url-loader',
+				options: urlLoaderOptions,
+			},
+			{
+				test: /\.svg$/,
+				include,
+				exclude,
+				loader: 'file-loader',
+				options: fileLoaderOptions,
+			},
+		],
+	},
+});
+
+exports.loadFonts = ({ include, exclude, urlLoaderOptions, fileLoaderOptions } = {}) => ({
+	module: {
+		rules: [
+			{
+				// Match woff2 in addition to patterns like .woff?v=1.1.1
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				include,
+				exclude,
+				use: {
+					loader: 'url-loader',
+					options: urlLoaderOptions,
+				},
+			},
+			{
+				test: /\.(eot|ttf)$/,
+				loader: 'file-loader',
+				options: fileLoaderOptions,
+			},
+		],
+	},
+});
+
+exports.loadTinyMCE = () => ({
+	module: {
+		rules: [
+			{
+				test: require.resolve('tinymce/tinymce'),
+				use: ['imports-loader?this=>window', 'exports-loader?window.tinymce']
+			},
+			{
+				test: /tinymce\/(themes|plugins)\//,
+				use: ['imports-loader?this=>window']
+			},
+		],
+	},
+});
+
+exports.generateSourceMaps = ({ type } = {}) => ({
+	devtool: type,
+});
+
+exports.minifyJS = ({ options } = {}) => ({
+	optimization: {
+		minimize: true
+	},
+});
+
+exports.minifyCSS = ({ options } = {}) => ({
+	plugins: [
+		new OptimizeCSSAssetsPlugin({
+			cssProcessor: cssnano,
+			cssProcessorOptions: options,
+			canPrint: false,
+		}),
+	],
+});
+
+exports.setEnvironmentVariables = (dotenv = {}) => ({
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				...dotenv
+			},
+		}),
+	],
+});
+
+exports.gZipCompression = () => ({
+	plugins: [
+		new CompressionPlugin({
+			algorithm: 'gzip',
+		}),
+	],
+});
+
+exports.clean = (path) => ({
+	plugins: [
+		new CleanWebpackPlugin([
+			path,
+		]),
+	],
+});
+
+exports.copy = (from, to) => ({
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from, to, ignore: ['*.html'] },
+		]),
+	],
+});
