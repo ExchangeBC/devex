78ebe6175a4abc16412efbc53e1ebb44add79bf8
Andrew Sutherland <sutherlanda@gmail.com>
2018-11-25 00:11:30 -0800

    BA-1023 - Add in live reload for server TS
    - Remove unused files

--- config/env/development.js
+++ config/env/development.js
@@ -49,42 +49,11 @@ var devConfig = {
   app: {
     title: defaultEnvConfig.app.title + ' - Development Environment'
   },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/facebook/callback'
-  },
-  twitter: {
-    clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-    clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-    callbackURL: '/api/auth/twitter/callback'
-  },
-  google: {
-    clientID: process.env.GOOGLE_ID || 'APP_ID',
-    clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/google/callback'
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_ID || 'APP_ID',
-    clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/linkedin/callback'
-  },
   github: {
     clientID: process.env.GITHUB_ID || 'APP_ID',
     clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
     callbackURL: '/api/auth/github/callback',
     personalAccessToken: process.env.GITHUB_ACCESS_TOKEN || 'GITHUB_ACCESS_TOKEN'
-  },
-  // github: {
-  //   clientID: process.env.GITHUB_ID || 'fbcf57c13acde5c036c6',
-  //   clientSecret: process.env.GITHUB_SECRET || 'c8f0654b5ce9e8af026116b10afa6dfba419f558',
-  //   callbackURL: '/api/auth/github/callback'
-  // },
-  paypal: {
-    clientID: process.env.PAYPAL_ID || 'CLIENT_ID',
-    clientSecret: process.env.PAYPAL_SECRET || 'CLIENT_SECRET',
-    callbackURL: '/api/auth/paypal/callback',
-    sandbox: true
   },
   shared: {
     owasp: {
