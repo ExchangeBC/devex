5c4dceacd4e05365860d7f4cc544efda0245a010
Andrew Sutherland <sutherlanda@gmail.com>
2018-12-15 19:31:19 -0800

    BA-1066 - Updating pipeline configuration

--- sonar-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sonar-scanner \
-  -Dsonar.projectKey=devex-local-1 \
-  -Dsonar.sources=./modules \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=63ea3e02e8876721627d0d6afc553f98af221932
