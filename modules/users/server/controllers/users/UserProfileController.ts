493abca9a04c4c90ca1c0507002f5ba4a2087c0a
Andrew Sutherland <sutherlanda@gmail.com>
2019-01-02 21:08:44 -0800

    BA-1069 - Extract class for profile editing

--- modules/users/server/controllers/users/UserProfileController.ts
+++ modules/users/server/controllers/users/UserProfileController.ts
@@ -274,7 +274,7 @@ class UserProfileController {
 			const id = req.user._id;
 			req.logout();
 			// res.redirect('/');
-			UserModel.remove({ _id: id }, err => {
+			UserModel.deleteOne({ _id: id }, err => {
 				if (err) {
 					res.status(500).send({
 						message: 'Unable to delete profile!'
