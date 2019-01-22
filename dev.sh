f3114f2adb093a3861df181f8aa827d82d171586
Andrew Sutherland <sutherlanda@gmail.com>
2019-01-22 14:23:56 -0800

    BA-1089 - Remove hardcode credentials

--- dev.sh
+++ dev.sh
@@ -28,20 +28,9 @@ docker run \
 -e "NODE_ENV=${NODE_ENV-development}" \
 -e "MONGO_SEED=${MONGO_SEED-true}" \
 -e "DISABLE_WATCH=${DISABLE_WATCH-}" \
--e "GITHUB_ID=3d819dee8be7237af9ee" \
--e "GITHUB_SECRET=e3f26152b2d04e7877e1a57a07ea1d6bab63da18" \
+-e "GITHUB_ID=your_oauth_github_id" \
+-e "GITHUB_SECRET=your_oauth_github_secret" \
 -e "MONGO_SEED_LOG_RESULTS=${MONGO_SEED_LOG_RESULTS-true}" \
 -e "DEVEX_PROD=${DEVEX_PROD-false}" \
 -e "DOMAIN=http://localhost:3030" \
 -ti --rm --link db_devex --name mean_devex mean/devex ${@:-bash}
-
-# after run sh dev.sh
-# and run MAILER_SERVICE_PROVIDER="gmail" MAILER_FROM="<email>" MAILER_EMAIL_ID="<email>" MAILER_PASSWORD="<password>" NODE_ENV=development  nodejs
-
-# -v `pwd`:/opt/mean.js \
-# -ti --rm --link db_devex mean/devex bash
-
-
-# user FT9uq4nbk8mxJdBEx67rGqAHSC9RYBd
-# admin dwJ2y1VFHhFUZdwCknys9C2BF4vD2tJgEQRe7p9
-
