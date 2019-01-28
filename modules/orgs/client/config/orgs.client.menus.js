aa709beb05240cf46b15ff241c77d51e0497669a
Andrew Sutherland <sutherlanda@gmail.com>
2019-01-28 02:06:45 -0800

    BA-1081 - Add in menu item for org admin
    - Remove unused card directive

--- modules/orgs/client/config/orgs.client.menus.js
+++ modules/orgs/client/config/orgs.client.menus.js
@@ -1,13 +1,24 @@
 (function () {
 	'use strict';
 
-	angular.module('orgs').run(['menuService', function (menuService) {
+	angular.module('orgs').run(['menuService', 'OrgService', function (menuService, OrgService) {
 		menuService.addMenuItem ('topbar', {
 			title: 'Companies',
 			state: 'orgs.list',
 			roles: ['*'],
 			icon: 'none',
 			position: 1
+		});
+
+		OrgService.myadmin().$promise.then(orgs => {
+			if (orgs.length > 0) {
+				orgs.forEach(org => {
+					menuService.addSubMenuItem('account', 'settings', {
+						title: org.name,
+						state: `orgs.view({ orgId: '${org._id}' })`
+					})
+				})
+			}
 		});
 	}]);
 
